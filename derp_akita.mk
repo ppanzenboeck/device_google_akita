863593761d29a64ed844dab19022a92a3db4186c
ppanzenboeck <philipp.panzenboeck1@gmail.com>
2024-08-07 00:32:20 +0200

    P.A.N.Z. Adapations (DerpFest)

--- derp_akita.mk
+++ derp_akita.mk
@@ -6,7 +6,7 @@
 
 # Inherit some common stuff
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Inherit device configuration
 DEVICE_CODENAME := akita
@@ -19,7 +19,7 @@ $(call inherit-product, $(DEVICE_PATH)/$(DEVICE_CODENAME)/device-lineage.mk)
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 8a
-PRODUCT_NAME := lineage_$(DEVICE_CODENAME)
+PRODUCT_NAME := derp_$(DEVICE_CODENAME)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
