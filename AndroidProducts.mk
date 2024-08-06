b16ef10a927c05a5e05ea56d922611b4417ecf5b
ppanzenboeck <philipp.panzenboeck1@gmail.com>
2024-08-07 00:32:20 +0200

    P.A.N.Z. Adapations (DerpFest)

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -18,7 +18,8 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_akita.mk \
     $(LOCAL_DIR)/aosp_akita_fullmte.mk \
     $(LOCAL_DIR)/factory_akita.mk \
-    $(LOCAL_DIR)/lineage_akita.mk
+    $(LOCAL_DIR)/derp_akita.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_akita-trunk_staging-userdebug
+    aosp_akita-trunk_staging-userdebug \
+    derp_akita-user
